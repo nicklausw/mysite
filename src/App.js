5abc4b12430f1c3d177da0c78ad73a3156941ee2
Nicc <nick@berlin.com>
2021-12-11 20:26:43 -0500

    more cards

--- src/App.js
+++ src/App.js
@@ -2,23 +2,19 @@ import React, { Component } from 'react'
 import $ from 'jquery'
 
 import '../css/mystyles.css'
-import headshot from './pictures/headshot.jpg'
 
+import headshot from './pictures/headshot.jpg'
+import githubLogo from './pictures/github-logo.png'
 
 function setDesktop() {
   if($(window).width() >= 1200) {
-    $(".nameText").addClass("has-text-left");
-    $(".nameText").addClass("no-height");
-    $(".subText").addClass("has-text-right");
-    $(".subText").addClass("mt-3");
+    $(".nameText").addClass("has-text-left no-height ml-2");
+    $(".subText").addClass("has-text-right mt-3 mr-2");
   } else {
-    $(".nameText").removeClass("has-text-left");
-    $(".nameText").removeClass("no-height");
-    $(".subText").removeClass("has-text-right");
-    $(".subText").removeClass("mt-3");
+    $(".nameText").removeClass("has-text-left no-height ml-2");
+    $(".subText").removeClass("has-text-right mt-3 mr-2");
   }
 }
-
 
 // need this for export
 if (typeof window !== 'undefined') {
@@ -65,6 +61,38 @@ function iamilike() {
   )
 }
 
+function githubCard() {
+  return (
+    <div>
+      <div className="media">
+        <div className="media-content">
+          <figure className="image mb-0">
+            <a href="https://github.com/nicklausw/">
+              <img style={{width:"15em"}} src={githubLogo}/>
+            </a>
+          </figure>
+        </div>
+      </div>
+
+      <div className="content is-size-5">
+        Take a look for yourself.
+      </div>
+    </div>
+  )
+}
+
+function coolCard() {
+  return (
+    <div>
+      <p className="is-size-3 has-text-weight-bold">There's nothing here right now.</p>
+
+      <div className="content is-size-5">
+        The site just looks better with 4 cards.
+      </div>
+    </div>
+  )
+}
+
 function Card(props) {
   return (
     <div className="column is-6">
@@ -80,20 +108,22 @@ function Card(props) {
 class App extends Component {
   render() {
     return (
-      <section className="section" id="page">
+      <section className="section pt-2 pb-4" id="page">
         <div className="container">
           <div className="columns is-centered">
             <div className="column is-9">
               <div className="content has-text-white has-text-centered">
                 <p className="is-size-1 m-0 nameText">Nicklaus Willis</p>
                 <p className="is-size-3 m-0 subText">Web Developer / <a href="javascript:location='mailto:\u006e\u0040\u006e\u0069\u0063\u006b\u006c\u0061\u0075\u0073\u0077\u002e\u0063\u006f\u006d';void 0">e-mail</a> / <a href="tel:7708289753">phone</a></p>
-                <figure className="image mb-0 is-inline-block mainImage">
+                <figure className="image mb-0 mt-4 is-inline-block mainImage">
                   <img className="is-rounded" src={headshot}/>
                 </figure>
                 <hr/>
-                <div className="columns is-vcentered">
+                <div className="columns is-vcentered is-multiline">
                   <Card content={haistenCard()}/>
                   <Card content={iamilike()}/>
+                  <Card content={coolCard()}/>
+                  <Card content={githubCard()}/>
                 </div>
               </div>
             </div>
