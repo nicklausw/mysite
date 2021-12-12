0415fe11336529179bf66af1b5ccb43b969987d2
Nicc <nick@berlin.com>
2021-12-11 20:37:09 -0500

    batch file builds css file if not present

--- setupDevEnvironment.bat
+++ setupDevEnvironment.bat
@@ -1,3 +1,5 @@
-@set mypath=%~dp0
-@start "Bulma CSS Updater" ""C:\Windows\System32\cmd.exe"" /k ""C:\Program Files\nodejs\nodevars.bat" && cd %mypath% && npm run css-watch"
-@start "Main Program Updater" ""C:\Windows\System32\cmd.exe"" /k ""C:\Program Files\nodejs\nodevars.bat" && cd %mypath% && npm start"
+@echo off
+set mypath=%~dp0
+if not exist %mypath%\css\NUL start "Bulma CSS First-time Builder" ""C:\Windows\System32\cmd.exe"" /k ""C:\Program Files\nodejs\nodevars.bat" && cd %mypath% && npm run css-build && exit"
+start "Bulma CSS Updater" ""C:\Windows\System32\cmd.exe"" /k ""C:\Program Files\nodejs\nodevars.bat" && cd %mypath% && npm run css-watch"
+start "Main Program Updater" ""C:\Windows\System32\cmd.exe"" /k ""C:\Program Files\nodejs\nodevars.bat" && cd %mypath% && npm start"
