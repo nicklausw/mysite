411e960f9e5297a644a8efd076973f53c24033ee
nicklausw <n@nicklausw.com>
2022-01-26 17:19:52 -0500

    import all of bulma; purgeCSS will handle bloat

--- buildSite.sh
+++ buildSite.sh
@@ -1,3 +1,4 @@
+export NODE_OPTIONS=--openssl-legacy-provider
 npm run build
 cd dist
-purgecss --content index.html about/index.html contact/index.html *.*.js templates/__react_static_root__/src/pages/*.*.js --css styles.*.css --output styles.*.css
+purgecss --content index.html */index.html *.*.js templates/__react_static_root__/src/pages/*.*.js --css styles.*.css --output styles.*.css
