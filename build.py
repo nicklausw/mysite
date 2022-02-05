d89bfc14c52ef4d1367c830f1e495324056affa0
nicklausw <n@nicklausw.com>
2022-02-04 19:43:50 -0500

    404 page; more stuff in public directory

--- build.py
+++ build.py
@@ -4,4 +4,4 @@ os.system("python3 blog.py")
 os.environ["NODE_OPTIONS"] = "--openssl-legacy-provider"
 os.system("npm run build")
 os.chdir(os.path.join(os.getcwd(), "dist"))
-os.system("purgecss --content index.html */index.html *.*.js templates/__react_static_root__/src/*/*.*.js --css styles.*.css --output styles.*.css")
+os.system("purgecss --content *.html */*.html *.*.js templates/__react_static_root__/src/*/*.*.js --css styles.*.css --output styles.*.css")
