19b4020881a8e84318825fbe8b41fe4d3ff5dd22
nicklausw <n@nicklausw.com>
2022-02-06 14:08:31 -0500

    purgecss windows compatibility

--- build.py
+++ build.py
@@ -1,7 +1,20 @@
 #!/usr/bin/env python3
 import os
+import glob
+
+# build blog database
 exec(compile(open("blog.py", "r").read(), "blog.py", "exec"))
+
+# build actual site (comment out below line for node v16)
 os.environ["NODE_OPTIONS"] = "--openssl-legacy-provider"
 os.system("npm run build")
-os.chdir(os.path.join(os.getcwd(), "dist"))
-os.system("purgecss --safelist \"pre\" --content *.html */*.html *.*.js templates/__react_static_root__/src/*/*.*.js --css styles.*.css --output styles.*.css")
+
+# run purgecss
+os.chdir("dist")
+contentList = " ".join(\
+  glob.glob("*.html") + \
+  glob.glob("*/*.html") + \
+  glob.glob("*.*.js") + \
+  glob.glob("templates/__react_static_root__/src/*/*.*.js"))
+cssFile = " ".join(glob.glob("styles.*.css"))
+print("purgecss --safelist \"pre\" --content " + contentList + " --css " + cssFile + " --output " + cssFile)
--- test.py
+++ test.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 import os
+
+# build blog database
 exec(compile(open("blog.py", "r").read(), "blog.py", "exec"))
+
+# build actual site (comment out below line for node v16)
 os.environ["NODE_OPTIONS"] = "--openssl-legacy-provider"
 os.system("npm start")
