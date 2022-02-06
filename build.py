2886901938b02b7ceaf492610b41f3e870dd9d6f
nicklausw <n@nicklausw.com>
2022-02-05 19:50:08 -0500

    safelist <pre> from purgecss

--- build.py
+++ build.py
@@ -4,4 +4,4 @@ os.system("python3 blog.py")
 os.environ["NODE_OPTIONS"] = "--openssl-legacy-provider"
 os.system("npm run build")
 os.chdir(os.path.join(os.getcwd(), "dist"))
-os.system("purgecss --content *.html */*.html *.*.js templates/__react_static_root__/src/*/*.*.js --css styles.*.css --output styles.*.css")
+os.system("purgecss --safelist \"pre\" --content *.html */*.html *.*.js templates/__react_static_root__/src/*/*.*.js --css styles.*.css --output styles.*.css")
