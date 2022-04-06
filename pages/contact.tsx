4672239b4ba02e6d9baf8b7bc5c80ad4f96ee879
nicklausw <n@nicklausw.com>
2022-04-05 21:21:36 -0400

    react-obfuscate breaks styling for some reason. replace with react-obfuscate-email

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,5 +1,5 @@
 import Header from '../components/Header'
-import Obfuscate from "react-obfuscate"
+import { Email } from "react-obfuscate-email";
 
 export default function Home() {
   return (
@@ -8,7 +8,7 @@ export default function Home() {
       <div className="section">
         <div className="container">
           <div className="content has-text-white">
-            <p className="is-size-5 has-text-centered pl-0">e-mail: <Obfuscate linkText="E-mail me!" email="n@nicklausw.com"/></p>
+            <p className="is-size-5 has-text-centered pl-0">e-mail: <Email email="n@nicklausw.com"/></p>
             <p className="is-size-5 has-text-centered pl-0">phone: <a href="tel:7708289753">7708289753</a></p>
           </div>
         </div>
