81f4764f62c826a4ada0bb4999557ade1a8244f5
nicklausw <n@nicklausw.com>
2022-04-08 16:40:12 -0400

    postcss: components -> utils

--- postcss.config.js
+++ postcss.config.js
@@ -18,7 +18,7 @@ module.exports = {
       {
         content: [
             './pages/**/*.{js,jsx,ts,tsx}',
-            './components/**/*.{js,jsx,ts,tsx}'
+            './utils/**/*.{js,jsx,ts,tsx}'
         ],
         defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
         safelist: ["html", "body"]
